53dbf4f0916e5f00e811bc455e0f8a52c74b6b56
권장순 <jsoonworld@gmail.com>
2025-07-17 16:26:17 +0900

    feat(Price) : Price vo 구현

---
+++ src/main/java/foodiepass/server/price/domain/Price.java
@@ -0,0 +1,31 @@
+package foodiepass.server.price.domain;
+
+import foodiepass.server.currency.domain.Currency;
+import foodiepass.server.price.exception.PriceErrorCode;
+import foodiepass.server.price.exception.PriceException;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@EqualsAndHashCode
+@RequiredArgsConstructor
+public class Price {
+
+    private final Currency currency;
+    private final Double amount;
+
+    public Price add(final Price other) {
+        if (!this.currency.equals(other.currency)) {
+            throw new PriceException(PriceErrorCode.CURRENCIES_DO_NOT_MATCH);
+        }
+        return new Price(this.currency, this.amount + other.amount);
+    }
+
+    public Price multiply(final int quantity) {
+        if (quantity < 0) {
+            throw new PriceException(PriceErrorCode.INVALID_QUANTITY);
+        }
+        return new Price(this.currency, this.amount * quantity);
+    }
+}
