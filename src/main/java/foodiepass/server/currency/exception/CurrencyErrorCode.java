04b43f55763661fbde153d6a8b761d310e515473
권장순 <jsoonworld@gmail.com>
2025-07-20 19:59:13 +0900

    feat(currency): CurrencyErrorCode에 INVALID_CURRENCY_INPUT 추가 및 메시지 형식 개선

--- src/main/java/foodiepass/server/currency/exception/CurrencyErrorCode.java
+++ src/main/java/foodiepass/server/currency/exception/CurrencyErrorCode.java
@@ -10,10 +10,10 @@ import org.springframework.http.HttpStatus;
 public enum CurrencyErrorCode implements ErrorCode {
 
     CURRENCY_NOT_FOUND(HttpStatus.NOT_FOUND, "유효하지 않은 통화입니다."),
-    ;
-    private static final String PREFIX = "[CURRENCY ERROR] ";
+    INVALID_CURRENCY_INPUT(HttpStatus.BAD_REQUEST, "통화 이름 또는 코드는 비어있을 수 없습니다.");
+
     private final HttpStatus status;
-    private final String rawMessage;
+    private final String message;
 
     @Override
     public HttpStatus getStatus() {
@@ -22,6 +22,6 @@ public enum CurrencyErrorCode implements ErrorCode {
 
     @Override
     public String getMessage() {
-        return PREFIX + rawMessage;
+        return message;
     }
 }
