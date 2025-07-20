9f9810d8c39e03a0810279358af74abdde3d75ff
권장순 <jsoonworld@gmail.com>
2025-07-20 20:02:47 +0900

    refactor(order): order 도메인 모델 및 예외 처리 리팩터링

--- src/main/java/foodiepass/server/order/domain/OrderItem.java
+++ src/main/java/foodiepass/server/order/domain/OrderItem.java
@@ -1,17 +1,44 @@
 package foodiepass.server.order.domain;
 
-import foodiepass.server.price.domain.Price;
+import foodiepass.server.common.price.domain.Price;
+import foodiepass.server.food.domain.MenuItem;
+import foodiepass.server.order.exception.OrderErrorCode;
+import foodiepass.server.order.exception.OrderException;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import java.util.Objects;
 
 @Getter
-@RequiredArgsConstructor
+@EqualsAndHashCode
 public class OrderItem {
 
     private final MenuItem menuItem;
     private final int quantity;
 
+    public OrderItem(final MenuItem menuItem, final int quantity) {
+        validateMenuItem(menuItem);
+        validateQuantity(quantity);
+        this.menuItem = menuItem;
+        this.quantity = quantity;
+    }
+
     public Price calculateTotalPrice() {
         return menuItem.getPrice().multiply(quantity);
+    }
+
+    public String getName() {
+        return menuItem.getName();
+    }
+
+    private void validateMenuItem(MenuItem menuItem) {
+        if (Objects.isNull(menuItem)) {
+            throw new OrderException(OrderErrorCode.NULL_MENU_ITEM);
+        }
+    }
+
+    private void validateQuantity(int quantity) {
+        if (quantity <= 0) {
+            throw new OrderException(OrderErrorCode.INVALID_ORDER_QUANTITY);
+        }
     }
 }
