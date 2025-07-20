ab7ac232ad0843b2b71b38b310f11e89a5ac9036
권장순 <jsoonworld@gmail.com>
2025-07-20 19:55:33 +0900

    chore(common): Price 관련 도메인을 common 패키지로 이동

--- src/main/java/foodiepass/server/price/exception/PriceException.java
+++
@@ -1,9 +0,0 @@
-package foodiepass.server.price.exception;
-
-import foodiepass.server.global.error.BaseException;
-
-public class PriceException extends BaseException {
-    public PriceException(PriceErrorCode errorCode) {
-        super(errorCode);
-    }
-}
