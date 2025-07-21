f0409cce5595a0ddb2f79e61d503f4f538da65e9
권장순 <jsoonworld@gmail.com>
2025-07-21 09:34:37 +0900

    refactor(language): 에러 메시지 명확화

--- src/main/java/foodiepass/server/language/exception/LanguageErrorCode.java
+++ src/main/java/foodiepass/server/language/exception/LanguageErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum LanguageErrorCode implements ErrorCode {
 
     LANGUAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "지원하지 않는 언어입니다."),
-    INVALID_LANGUAGE_INPUT(HttpStatus.BAD_REQUEST, "언어 이름 또는 코드는 비어있을 수 없습니다.");
+    INVALID_LANGUAGE_INPUT(HttpStatus.BAD_REQUEST, "언어 입력값이 유효하지 않습니다."),
+    TRANSLATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "텍스트 번역 중 오류가 발생했습니다.");
     ;
 
     private final HttpStatus status;
