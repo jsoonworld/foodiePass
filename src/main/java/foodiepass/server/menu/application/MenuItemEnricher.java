35cd92c5c3b7d35cc40bcf793e4e5fe5c80fe0a5
권장순 <jsoonworld@gmail.com>
2025-07-24 14:43:12 +0900

    feat(menu): MenuItemEnricher 리액티브 체인에 에러 핸들링 추가

--- src/main/java/foodiepass/server/menu/application/MenuItemEnricher.java
+++ src/main/java/foodiepass/server/menu/application/MenuItemEnricher.java
@@ -5,15 +5,18 @@ import foodiepass.server.currency.domain.Currency;
 import foodiepass.server.language.domain.Language;
 import foodiepass.server.menu.application.port.out.FoodScrapper;
 import foodiepass.server.menu.application.port.out.TranslationClient;
+import foodiepass.server.menu.domain.FoodInfo;
 import foodiepass.server.menu.domain.MenuItem;
 import foodiepass.server.menu.dto.response.ReconfigureResponse.FoodItemResponse;
 import foodiepass.server.menu.dto.response.ReconfigureResponse.PriceInfoResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class MenuItemEnricher {
 
@@ -40,16 +43,28 @@ public class MenuItemEnricher {
             final Currency originCurrency,
             final Currency userCurrency
     ) {
-        Mono<String> engNameMono = translationClient.translateAsync(originLanguage, ENGLISH, menuItem.getName());
+        Mono<String> engNameMono = translationClient.translateAsync(originLanguage, ENGLISH, menuItem.getName())
+                .onErrorResume(e -> {
+                    log.warn("영문 번역 실패: '{}'. 원본 이름을 사용합니다.", menuItem.getName(), e);
+                    return Mono.just(menuItem.getName());
+                });
 
         return engNameMono.flatMap(engName ->
                 foodScraper.scrapAsync(List.of(engName))
                         .next()
+                        .onErrorResume(e -> {
+                            log.warn("스크래핑 실패: '{}'. 기본 FoodInfo를 사용합니다.", engName, e);
+                            return Mono.just(new FoodInfo(engName, "상세 정보를 불러올 수 없습니다.", "", ""));
+                        })
                         .flatMap(scrapedFoodInfo -> {
-                            Mono<String> translatedNameMono = translationClient.translateAsync(ENGLISH, userLanguage, scrapedFoodInfo.getName());
-                            Mono<String> translatedDescriptionMono = translationClient.translateAsync(ENGLISH, userLanguage, scrapedFoodInfo.getDescription());
+                            Mono<String> translatedNameMono = translationClient.translateAsync(ENGLISH, userLanguage, scrapedFoodInfo.getName())
+                                    .onErrorResume(e -> Mono.just(scrapedFoodInfo.getName()));
 
-                            Mono<PriceInfoResponse> priceInfoMono = currencyService.convertAndFormatAsync(menuItem.getPrice(), userCurrency);
+                            Mono<String> translatedDescriptionMono = translationClient.translateAsync(ENGLISH, userLanguage, scrapedFoodInfo.getDescription())
+                                    .onErrorResume(e -> Mono.just("상세 설명을 번역할 수 없습니다."));
+
+                            Mono<PriceInfoResponse> priceInfoMono = currencyService.convertAndFormatAsync(menuItem.getPrice(), userCurrency)
+                                    .onErrorResume(e -> Mono.just(new PriceInfoResponse("N/A", "N/A")));
 
                             return Mono.zip(translatedNameMono, translatedDescriptionMono, priceInfoMono)
                                     .map(tuple -> new FoodItemResponse(
