c60251d8f4f3101d2d9ca50cedc5417b25f062d7
권장순 <jsoonworld@gmail.com>
2025-07-26 20:18:10 +0900

    refactor(currency): 환율 계산 API 비동기 처리로 전환

--- src/main/java/foodiepass/server/currency/api/CurrencyController.java
+++ src/main/java/foodiepass/server/currency/api/CurrencyController.java
@@ -8,25 +8,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/currency")
 public class CurrencyController {
 
     private final CurrencyService currencyService;
 
-    @GetMapping("/currency")
     public List<CurrencyResponse> getCurrencies() {
         return currencyService.findAllCurrencies();
     }
 
-    @PostMapping("/currency/calculate")
-    public CalculatePriceResponse calculateTotalPrice(
+    @PostMapping("/calculate")
+    public Mono<CalculatePriceResponse> calculateTotalPrice(
             @RequestBody final CalculatePriceRequest calculatePriceRequest
     ) {
-        return currencyService.calculateOrdersPrice(calculatePriceRequest);
+        return currencyService.calculateOrdersPriceAsync(calculatePriceRequest);
     }
 }
